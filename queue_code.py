3ceb8933134a11f1b7e2c1f5a8acf0b445865e50
Rudri Trivedi <rudritrivedi28@gmail.com>
2024-08-09 16:56:19 +0530

    delete element code added

--- queue_code.py
+++ queue_code.py
@@ -10,15 +10,28 @@ class creat_queue:
         value_append_in_queue = input("Entar a Value :")
         self.queue.append(value_append_in_queue)
 
+    def delete_in_queue(self):
+        self.queue.pop(0)
+
     def user_input(self):
         while True:
-            print("\n1-Inser a element .")
+            print("\n1-Inser a element.")
+            print("2-delete a element.")
+            print("3-Exit from queue.\n")
 
             user_input = input("Entar your choice ..")
 
             if user_input == "1":
                 self.insert_in_queue()
                 self. __repr__()
+
+            if user_input=='2':
+                self.delete_in_queue()
+                self.__repr__()
+            
+            if user_input == '3':
+                break
+
 
 queue = creat_queue()
 queue.user_input()
